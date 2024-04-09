fe4d0de1e54594b23b7a3a4a65cbbfef94ea9049
aquinoctium <bubblecoco.aj@gmail.com>
2024-04-09 16:29:25 -0400

    added member searching by name feature

--- trainer.js
+++ trainer.js
@@ -21,7 +21,7 @@ async function login() {
   const res = await performQuery(command, values);
 
   if (res && res.rows.length > 0) {
-    console.log("\n\nSuccessful Login!\nWELCOME " + res.rows[0].first_name +  + " " + res.rows[0].last_name + "\n");
+    console.log("\n\nSuccessful Login!\nWELCOME " + res.rows[0].first_name  + " " + res.rows[0].last_name + "\n");
     displayTrainerMenu();
   } else {
     console.log("Login unsuccessful! Please try again.");
@@ -30,14 +30,40 @@ async function login() {
 }
 
 async function displayTrainerMenu() {
-  console.log("TRAINER MENU: \n");
+  console.log("\nTRAINER MENU: \n");
   console.log("1 - Schedule Management");
   console.log("2 - Member Profile Viewing" );
   console.log("0 - Exit");
 
   let choice = await question("Enter your choice or 0 to Exit: ", answer => ['1', '2'].includes(answer));
+  if (choice == 1) {
+    // schedule management 
+  } else if (choice == 2) {
+    membersearching(); 
+  }
+}
+async function membersearching(){
+  let fullName = await question("Enter Member's Name: ");
+  const flname = fullName.split(" ")
+  const command = 'SELECT * FROM members WHERE first_name = $1 AND last_name = $2';
+  const data = await performQuery(command, flname);
+  try {
+    if (data.rows.length > 0) {
+      data.rows.forEach((item) => {
+        console.log(`\nMemberID: ${item.member_id}`);
+        console.log(`Email: ${item.email}`);
+        console.log(`First Name: ${item.first_name}`);
+        console.log(`Last Name: ${item.last_name}`);
+      });;
+    } else {
+      console.log("Unable to find specified member")
+    }
+} catch (error) { // another error message gets printed, needs to be fixed..? from perform query
+  console.log("Unable to find specified member")
+}
+  displayTrainerMenu();
 }
 
 
 
-module.exports = { createAccount, login};
+module.exports = { createAccount, login, displayTrainerMenu};
