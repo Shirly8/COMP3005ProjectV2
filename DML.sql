e4d6ca32b9f44d460fc76a518e5f519a2abe9b6e
aquinoctium <bubblecoco.aj@gmail.com>
2024-04-09 16:07:26 -0400

    adding test billing data

--- DML.sql
+++ DML.sql
@@ -10,3 +10,6 @@ INSERT INTO staff (email, password, first_name, last_name) VALUES ('trainer2@exa
 INSERT INTO dashboard (member_id, exercise_routines, fitness_goals, health_metrics) VALUES (1, 'Monday: Cardio, Tuesday: Upper body workout, Wednesday: Rest', 'Lose 10 pounds in 2 months', 'Weight: 160 lbs, Blood pressure: 120/80');
 INSERT INTO dashboard (member_id, exercise_routines, fitness_goals, health_metrics) VALUES (2, 'Monday: Lower body workout, Tuesday: Yoga, Wednesday: Cardio', 'Increase muscle mass by 5%', 'Weight: 180 lbs, Blood pressure: 130/85');
 INSERT INTO dashboard (member_id, exercise_routines, fitness_goals, health_metrics) VALUES (3, 'Monday: Rest, Tuesday: HIIT, Wednesday: Upper body workout', 'Improve endurance and stamina', 'Weight: 150 lbs, Blood pressure: 115/75');
+INSERT INTO billing (member_id, amount, due_date, paid) VALUES (1, 60, '2024-04-15', false);
+INSERT INTO billing (member_id, amount, due_date, paid) VALUES (2, 60, '2024-04-20', false);
+INSERT INTO billing (member_id, amount, due_date, paid) VALUES (3, 60, '2024-04-25', true);
